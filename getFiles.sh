198bec7948f32d582b84daf300284e7fd01057e0
Chandanpanda <Chandanpanda2006@yahoo.com>
2017-02-25 20:21:05 +0530

    Add files via upload

---
+++ getFiles.sh
@@ -0,0 +1,23 @@
+pip install kaggle-cli
+
+sudo apt-get install unzip
+
+kg config -g -u "ChandanPanda2006" -p "Passw0rd" -c "the-nature-conservancy-fisheries-monitoring"
+
+kg download
+
+unzip train.zip
+
+mkdir test
+
+unzip test_stg1.zip -d "test"
+
+mkdir results
+
+mkdir valid
+
+rm -rf train/__MACOSX
+
+rm train/.DS_Store
+
+rm -rf test/test_stg1/__MACOSX
